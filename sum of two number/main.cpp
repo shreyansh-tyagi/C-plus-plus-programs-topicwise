cbb92153d9d3d113384c9698953b42a64272ed36
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-20 01:31:04 +0530

    sum and difference of two number

--- sum of two number/main.cpp
+++ sum of two number/main.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
-    int a,b,sum=0;
+    int a,b,c,sum=0,x,y;
     cout<<"enter the value of a and b: ";
-    cin>>a>>b;
-    //cin>>b;
+    cin>>a>>b>>c;
+    cout<<"enter the value of x and y: ";
+    cin>>x>>y;
     sum=a+b;
-    cout<<"the sum of two number is : "<<sum<<endl;
+    cout<<"sum: "<<sum<<endl;
+    cout<<"the sum of three(a+b+c) number is : "<<a+b+c<<endl;
+    cout<<"the difference of two number(x-y) is : "<<x-y<<endl;
     return 0;
 
 }
