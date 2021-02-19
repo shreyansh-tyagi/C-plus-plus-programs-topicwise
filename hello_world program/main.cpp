5ba2b522ff3987a9f9558832b790c1d92fbf2a50
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-20 00:51:05 +0530

    hello world program

--- hello_world program/main.cpp
+++ hello_world program/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
-void main()
+int main()
 {
-    cout<<"hello world let's start c++ programming from today"<< endl;
+    cout<<"hello world let's start c++ programming from today\ntoday date is :- 20-02-21 from which i have started\nlearning C++"<< endl;
+
+    return 0;
 }
