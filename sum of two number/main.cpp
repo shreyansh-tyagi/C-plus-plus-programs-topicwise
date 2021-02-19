3c4ad9723f2fd3b15522b34ac2607ab25740b367
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-20 01:25:42 +0530

    sum of two number

--- sum of two number/main.cpp
+++ sum of two number/main.cpp
@@ -4,8 +4,10 @@ int main()
 {
     int a,b,sum=0;
     cout<<"enter the value of a and b: ";
-    cin>>a,b;
+    cin>>a>>b;
+    //cin>>b;
     sum=a+b;
     cout<<"the sum of two number is : "<<sum<<endl;
+    return 0;
 
 }
