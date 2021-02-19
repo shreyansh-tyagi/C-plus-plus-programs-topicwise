5b0ba95632d1176d4468975ccda836b797dd53e6
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-20 01:22:22 +0530

    sum of two number

---
+++ sum of two number/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"enter the value of a and b: ";
+    cin>>a,b;
+    sum=a+b;
+    cout<<"the sum of two number is : "<<sum<<endl;
+
+}
