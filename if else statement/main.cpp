899caa1162dbd5b3f04c4bc3c22304c1f623ddb9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-21 23:37:02 +0530

    if statement program to find the largest among three number

--- if else statement/main.cpp
+++ if else statement/main.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main()
 {
     int a,b,c;
-    cout<<"enter the value a,b,c";
+    cout<<"enter the value a,b,c: ";
     cin>>a>>b>>c;
+    if(a>b)&&(a>c)
+    {
+        cout<<"a is greater";
+    }
+    else if(b>a)&&
+
     return 0;
 }
