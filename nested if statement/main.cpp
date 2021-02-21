62b8637003e3079e6c1f704d180579fc8f4647f7
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-21 23:49:24 +0530

    if statement program to find the largest among three number

--- nested if statement/main.cpp
+++ nested if statement/main.cpp
@@ -5,4 +5,15 @@ int main()
     int a,b,c;
     cout<<"enter the value of a,b,c";
     cin>>a>>b>>c;
+    if(a>b)
+    {
+        if(b>c)
+        {
+            cout<<"a is greater";
+        }
+
+    }
+    else if(b>c){
+        cout<<"b is greater";
+    }
 }
