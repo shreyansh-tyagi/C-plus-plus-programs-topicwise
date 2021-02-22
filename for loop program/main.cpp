90225f1fb0c71725550efde235c6c70a8c93b1f8
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-22 23:47:17 +0530

    for loops program has been added

---
+++ for loop program/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        
+    }
+    return 0;
+}
