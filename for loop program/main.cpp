5d56520f1361524efa4961095b0474c91c431210
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-22 23:52:04 +0530

    for loops program has been added

--- for loop program/main.cpp
+++ for loop program/main.cpp
@@ -6,12 +6,17 @@
 using namespace std;
 int main()
 {
-    int i,n;
+    int i,j,n;
     cout<<"enter the value of n: ";
     cin>>n;
     for(i=0;i<n;i++)
     {
-        
+       for(j=0;j<=i;j++)
+       {
+           cout<<"*";
+
+       } 
+       cout<<"\n";
     }
     return 0;
 }
