10bb4de294a33f7caf878ea8c6c1b0258cc86d54
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-25 18:50:21 +0530

    diamond pattern

---
+++ diamond pattern/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int i,j,k,l,n;
+    cout<<"enter the size of diamond: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<" ";
+        }
+        for(k=0;k<i;k++)
+        {
+            cout
+        }
+    }
+    return 0;
+}
