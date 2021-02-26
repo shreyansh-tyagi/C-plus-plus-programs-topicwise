53a0e149eb221ab295c836a7dfd550e321b02077
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-02-26 22:58:09 +0530

    Update C++Solution.cpp

--- Reverse Digits in Number/C++Solution.cpp
+++ Reverse Digits in Number/C++Solution.cpp
@@ -1,24 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int reversDigit(int num)
-{
-    int rev = 0;
-    while(num > 0)
-    {
-        rev = rev*10 + num%10;
-        num = num/10;
-    }
-    return rev;
-}
- 
-
 int main()
 {
-    int num ;
+  int num ;
   cin>>num;
-    cout << "Reverse of no. is "
-         << reversDigit(num);
-
+  
+  int rev = 0 , digit;
+   
+  while(num > 0)
+    {   
+        digit= num%10;              //digit stores the last digit of the number as '% 10 ' returns the remainder of the 'number / 10' which is the last digit
+        rev = (rev*10) + digit ;    //rev is now stored the last digit of the number as the first number
+        num = num/10;               //num  now contains the remaining digits if the number excluding the last digit
+    }
+    cout << "Reverse of the number is " << rev;
     return 0;
 }
