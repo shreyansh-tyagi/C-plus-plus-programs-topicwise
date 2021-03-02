8020bb43260ef4b17f4a246736d028f4cad586bb
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-02 19:23:49 +0530

    additon sub,multipliaction and division through function has been added

--- addition ,substraction through function/main.cpp
+++ addition ,substraction through function/main.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int add(int,int);
 int sub(int,int);
 int mul(int,int);
-//int div(int,int);
+void divi(int,int);
 int main()
 {
-    int a ,b,c,d,e,f;
+    int a,b,c,d,e,f;
    // float f;
     cout<<"enter two numbers : \n";
     cin>>a>>b;
     c=add(a,b);
     d=sub(a,b);
     e=mul(a,b);
-    //f=div(a,b);
+    divi(a,b);
     cout<<"sum: "<<c<<endl<<"subtarction: "<<d<<endl<<"multiplication: "<<e<<endl;
     return 0;
 }
@@ -33,7 +33,17 @@ int mul(int a ,int b)
 {
     return (a*b);
 }
-/*int div(int a ,int b)
+void divi(int a ,int b) //the function DIV is a function through which we will find the quotient on dividing the two inputs after checking which is larger using the '/' operator
 {
-    return (abs(a/b));
-}*/
+    int d;
+ if(a<b)
+ {
+	 d=b/a;
+	 cout<<"\nDivision of "<<" "<<b<<" "<<"by"<<" "<<a<<" "<<"is :" <<d;
+ }
+ else
+ {
+   d=a/b;
+	 cout<<"\nDivision of "<<" "<<a<<" "<<"by"<<" "<<b<<" "<<"is :" <<d;
+ }
+}
