26614f9b39482967a2103ec6fb5170142b434b5d
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-02 19:10:06 +0530

    additon sub,multipliaction and division through function has been added

--- addition ,substraction through function/main.cpp
+++ addition ,substraction through function/main.cpp
@@ -10,9 +10,18 @@ int mul(int,int);
 float div(int,int);
 int main()
 {
-    int a ,b;
+    int a ,b,c,d,e;
+    float f;
     cout<<"enter two numbers : \n";
     cin>>a>>b;
-    
+    c=add(a,b);
+    d=sub(a,b);
+    e=mul(a,b);
+    f=div(a,b);
+    cout<<"sum: "<<c<<endl<<"subtarction: "<<d<<endl;
     return 0;
+}
+int add(int a ,int b)
+{
+    return (a+b);
 }
