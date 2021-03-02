34e1d70641ea0cdfbef551fa25ac44d84ad35dfd
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-02 18:54:18 +0530

    additon sub,multipliaction and division through function has been added

---
+++ addition ,substraction through function/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int add(int,int);
+int sub(int,int);
+int mul(int,int);
+float div(int,int);
+int main()
+{
+    
+    return 0;
+}
