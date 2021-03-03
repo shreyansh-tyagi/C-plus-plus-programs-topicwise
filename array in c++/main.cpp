0a632686af3184f35d93f4cf07b46a6fe1565298
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-03 21:02:14 +0530

    array in c++ to calculate the sum of n number

---
+++ array in c++/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int sum,i,n,a[50];
+    cout<<"enter the size of array: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        
+    }
+    return 0;
+}
