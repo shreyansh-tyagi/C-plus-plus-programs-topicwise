1dc9e34231fdc07c4c2e547c9fa9d1e07131a1b9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-04 21:01:29 +0530

    array reverese program has been added

---
+++ array reverse/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int i,n,a[50];
+    cout<<"enter the size of array: ";
+    cin>>n;
+    for(i =0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(i =0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    return 0;
+}
