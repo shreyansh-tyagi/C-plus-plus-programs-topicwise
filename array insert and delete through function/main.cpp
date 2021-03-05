1351f2f54fdeb52d843b3a5a61b4ed2f0fea8a59
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-06 02:10:11 +0530

    array insert and delete through function

---
+++ array insert and delete through function/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void insert(int [],int);
+void delete(int [],int)
+int main()
+{
+    int
+    return 0;
+}
