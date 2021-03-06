202d76859bd551c3ad06be0d71c620f9fcf6cee2
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-06 21:18:56 +0530

    array insert and delete through function

--- sorting in array/main.cpp
+++ sorting in array/main.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 void ascending(int [],int);
-//void descending(int [],int);
+void descending(int [],int);
 int main()
 {
     int i,n,a[50];
@@ -41,6 +41,28 @@ void ascending(int a[50],int n)
         }
     }
     cout<<"\nthe array in ascending : ";
+    for(i=1;i<=n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+void descending(int a[50],int n)
+{
+    int i,j,temp=0;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+
+        }
+    }
+    cout<<"\nthe array in desending : ";
     for(i=1;i<=n;i++)
     {
         cout<<a[i]<<" ";
