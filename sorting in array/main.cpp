d6dc5f045ae2586e370009ef6cfc6c425b1b967c
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-06 21:04:12 +0530

    array insert and delete through function

--- sorting in array/main.cpp
+++ sorting in array/main.cpp
@@ -25,3 +25,20 @@ int main()
     return 0;
 }
 void ascending(int a[50],int n)
+{
+    int i,j,temp=0;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=a[i];
+            }
+
+        }
+    }
+    cout
+}
