2d5fc4950debf6035caf06bf112639cb21d135d2
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-06 20:58:04 +0530

    sorting in array has been added

--- sorting in array/main.cpp
+++ sorting in array/main.cpp
@@ -8,6 +8,20 @@ void ascending(int [],int);
 void descending(int [],int);
 int main()
 {
-    
+    int i,n,a[50];
+    cin>>n;
+    cout<<"enter the size of array : ";
+    for(i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"first array : ";
+    for(i=1;i<=n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    ascending(a,n);
+    descending(a,n);
     return 0;
 }
+void ascending(int a[50],int n)
