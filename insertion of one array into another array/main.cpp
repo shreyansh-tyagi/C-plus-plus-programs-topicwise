9ce6318bea5423d59b6d6d270893e1d980fc5803
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-07 21:26:14 +0530

    sorting of array has been done

--- insertion of one array into another array/main.cpp
+++ insertion of one array into another array/main.cpp
@@ -8,7 +8,7 @@ void insertion(int [],int);
 int main()
 {
     int i,n,a[50];
-    cout<<"enter the size of array: ";
+    cout<<"\nenter the size of first array: ";
     cin>>n;
     for(i=1;i<=n;i++)
     {
@@ -18,4 +18,15 @@ int main()
     insertion(a,n);
     
     return 0;
+}
+void insertion(int a[50],int n)
+{
+    int i,b[50],m;
+    cout<<"\nenter the size of second array: ";
+    cin>>m;
+    for(i=1;i<=m;i++)
+    {
+        cin>>b[i];
+    }
+    cout<<"\nsecond array : ";
 }
