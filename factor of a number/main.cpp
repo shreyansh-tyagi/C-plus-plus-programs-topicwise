be7a53b50061108fa8bd1304adcedd0c29a2f0ac
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-08 02:39:28 +0530

    factor of a number has been added

---
+++ factor of a number/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"\nenter the number of which you want to find the factor: ";
+    cin>>n;
+    while(i<=n)
+    
+    return 0;
+}
