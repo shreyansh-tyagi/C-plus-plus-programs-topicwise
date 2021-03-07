2e475478e6b34a524aaab59071a5cb44b043f8d5
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-07 21:23:04 +0530

    insertion of one array into another array

--- insertion of one array into another array/main.cpp
+++ insertion of one array into another array/main.cpp
@@ -7,13 +7,15 @@ using namespace std;
 void insertion(int [],int);
 int main()
 {
-    int i,n;
+    int i,n,a[50];
     cout<<"enter the size of array: ";
     cin>>n;
     for(i=1;i<=n;i++)
     {
         cout<<a[i]<<" ";
     }
+    cout<<"\nfirst array : ";
+    insertion(a,n);
     
     return 0;
 }
