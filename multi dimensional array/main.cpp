27a98af5418390ad92b0ccd01aed3035df77fb6e
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-09 20:03:14 +0530

    multi dimensional array has been added

---
+++ multi dimensional array/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void multiarray(int [][],int)
+int main()
+{
+    int i ,n,a[50][50];
+    cout<<"enter the row and coloumn : ";
+    cin>>n;
+    return 0;
+}
