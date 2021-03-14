2ee2a510157fb1653b287265e32c0ee7ba2160d8
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-14 15:50:06 +0530

    pointer in c++

--- pointer in c++/main.cpp
+++ pointer in c++/main.cpp
@@ -9,9 +9,9 @@ int main()
     int i,*p;
     cout<<"enter the value to access through pointer: ";
     cin>>i;
-    *p=&i;
+    p=&i;
     cout<<"\nthe value of i is :  "<<i<<endl;
     cout<<"\n the value of p is : "<<p<<endl;
-    cout<<"\nthe va"
+    cout<<"\nthe value through pointer is : "<<*p<<endl;
     return 0;
 }
