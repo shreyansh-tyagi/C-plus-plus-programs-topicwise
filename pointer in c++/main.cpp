87adf9dba3f71d47366607d122912b2a94a2e73a
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-14 15:46:19 +0530

    pointer in c++

---
+++ pointer in c++/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int i,*p;
+    cout<<"enter the value to access through pointer: ";
+    return 0;
+}
