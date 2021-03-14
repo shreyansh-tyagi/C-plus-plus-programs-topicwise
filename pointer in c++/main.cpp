9e4788649d51e17c40bd188a0c446a0ddaf2bd73
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-14 15:49:03 +0530

    array reverse

--- pointer in c++/main.cpp
+++ pointer in c++/main.cpp
@@ -8,5 +8,10 @@ int main()
 {
     int i,*p;
     cout<<"enter the value to access through pointer: ";
+    cin>>i;
+    *p=&i;
+    cout<<"\nthe value of i is :  "<<i<<endl;
+    cout<<"\n the value of p is : "<<p<<endl;
+    cout<<"\nthe va"
     return 0;
 }
