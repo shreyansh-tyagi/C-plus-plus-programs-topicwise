7a1c2061ded72fe9a996cc7a9505d93571ab31e2
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-15 23:42:39 +0530

    pinter in array

--- pointer in array/main.cpp
+++ pointer in array/main.cpp
@@ -6,12 +6,18 @@
 using namespace std;
 int main()
 {
-    int i,a[50],*p,n;
+    int i,a[50],*p,n,*ptr[50];
     cout<<"enter the size of array: ";
     cin>>n;
+    cout<<"\nenter the value into array: ";
     for(i=0;i<n;i++)
     {
-        
+        cin>>a[i];
+    }
+    p=a;
+    for(i=0;i<n;i++)
+    {
+        cout<<
     }
     return 0;
 }
