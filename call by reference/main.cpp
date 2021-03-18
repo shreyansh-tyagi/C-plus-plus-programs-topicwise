2a6d88bc4c6d0baf221d0a48551b78884d308331
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-18 14:58:18 +0530

    call by reference

---
+++ call by reference/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int swap(int,int);
+int main()
+{
+    int a,b;
+    
+    return 0;
+}
