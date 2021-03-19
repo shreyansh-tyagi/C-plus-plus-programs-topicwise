a0b8a0e16f60ca3afd1470f694bdd0f58717b79e
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-19 22:56:25 +0530

    call by reference

--- function pointer/main.cpp
+++ function pointer/main.cpp
@@ -12,6 +12,16 @@ int main()
     cin>>a>>b;
     void (*function_pointer)(int *,int *);
     function_pointer=&function;
-    function_pointer(&a,&b); 
+    function_pointer(&a,&b);
+    cout<<"the value of a and b: "<<a<<" "<<b<<endl; 
     return 0;
+}
+void function(int *a,int *b)
+{
+    int temp=0,c;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    c=*a+*b;
+    cout
 }
