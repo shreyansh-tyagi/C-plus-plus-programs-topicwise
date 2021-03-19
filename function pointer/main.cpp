9a0b20497227c1c26c999dbfb613033dfc4df885
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-19 23:13:35 +0530

    call by reference

--- function pointer/main.cpp
+++ function pointer/main.cpp
@@ -10,10 +10,10 @@ int main()
     int a,b,c;
     cout<<"\nenter the value of a and b:\n";
     cin>>a>>b;
+    cout<<"the value of a and b: "<<a<<" "<<b<<endl; 
     void (*function_pointer)(int *,int *,int *);
     function_pointer=&function;
     function_pointer(&a,&b,&c);
-    cout<<"the value of a and b: "<<a<<" "<<b<<endl; 
     cout<<"the value of a and b after swapp: "<<a<<" "<<b<<endl;
     cout<<"the sum of a and b: "<<c<<endl;
     return 0;
