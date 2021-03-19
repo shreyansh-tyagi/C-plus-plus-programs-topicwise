f739e33b18d1afa16df5c7e31873b8615bcc4574
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-19 23:09:14 +0530

    call by reference

--- function pointer/main.cpp
+++ function pointer/main.cpp
@@ -4,28 +4,28 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-void function(int * , int * );
+void function(int * , int *,int * );
 int main()
 {
-    int a,b;
+    int a,b,c;
     cout<<"\nenter the value of a and b:\n";
     cin>>a>>b;
-    void (*function_pointer)(int *,int *);
+    void (*function_pointer)(int *,int *,int *);
     function_pointer=&function;
-    function_pointer(&a,&b);
+    function_pointer(&a,&b,&c);
     cout<<"the value of a and b: "<<a<<" "<<b<<endl; 
     cout<<"the value of a and b after swapp: "<<a<<" "<<b<<endl;
     return 0;
 }
-void function(int *a,int *b)
+void function(int *a,int *b,int *c)
 {
-    int temp=0,c;
+    int temp=0;
     temp=*a;
     *a=*b;
     *b=temp;
-    c=*a+*b;
+    *c=*a+*b;
     cout<<"the value of a and b in function:"<<a<<" "<<b<<endl;
     cout<<"swapped value of a and b:"<<*a<<" "<<*b<<endl;
-    cout<<"the sum of a and b: "<<c<<endl;
+    cout<<"the sum of a and b: "<<*c<<endl;
     
 }
