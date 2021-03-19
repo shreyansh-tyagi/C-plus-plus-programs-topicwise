7d6e7c64e8702807c18cf43c237fc4d5ae88bd6a
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-19 22:45:46 +0530

    function pointer program has been added

---
+++ function pointer/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int function_pointer(int , int );
+int main()
+{
+    int i,*p;
+    cout<<"\nenter the value of a and b:\n";
+    
+    return 0;
+}
