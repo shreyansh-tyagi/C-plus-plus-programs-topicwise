9b5f408ef7fc4b36ec623f419779e6e3d42aa4b9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-25 23:58:44 +0530

    function pointer in c++

---
+++ ABCD matrix array/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    char a[50][50],i,j,r1,c1;
+    cout<<'enter the row and coloumn:';
+    cin>>r1>>c1;
+    
+    return 0;
+}
