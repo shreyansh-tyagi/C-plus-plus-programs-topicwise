37a16d8679f61c5b396f23897b2fbf3b6329ff89
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-20 15:15:14 +0530

    cout and cin basic program

---
+++ cout and cin program/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    char arr[]='welcome to the world of c++';
+    
+    return 0;
+}
