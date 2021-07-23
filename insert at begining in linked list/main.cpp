2a0dd432f4adc45dfa52dfb25b163a92c5550e95
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-23 23:08:20 +0530

    insert at begining in linked list

---
+++ insert at begining in linked list/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main()
+{
+    int i,n,a;
+    cout<<"enter the size of linked list"
+    
+    return 0;
+}
