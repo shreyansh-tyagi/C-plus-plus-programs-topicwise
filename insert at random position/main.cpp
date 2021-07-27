0a618455709801f17a7e93486fcf5e34c824e864
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-27 18:04:03 +0530

    insert at random position

--- insert at random position/main.cpp
+++ insert at random position/main.cpp
@@ -5,10 +5,13 @@
 #include <algorithm>
 using namespace std;
 struct node{
+    int data;
+    struct node *link;
 
 };
 int main()
 {
-    
+    struct node *temp=NULL,*root=NULL,*p=NULL;
+    cout<<"enter the length of linked list: ";
     return 0;
 }
