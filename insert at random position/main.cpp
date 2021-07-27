86b3a449b3d2523100672fce586476c8318cc79b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-27 18:07:05 +0530

    insert at random position

--- insert at random position/main.cpp
+++ insert at random position/main.cpp
@@ -11,7 +11,14 @@ struct node{
 };
 int main()
 {
+    int i,n;
     struct node *temp=NULL,*root=NULL,*p=NULL;
     cout<<"enter the length of linked list: ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        temp=(struct node *)malloc(sizeof(struct node));
+        
+    }
     return 0;
 }
