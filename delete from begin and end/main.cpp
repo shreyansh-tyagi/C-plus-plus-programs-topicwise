5b90384a52364450e7b4b613db2354d5dfb0417b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-31 17:01:55 +0530

    delete from begin and end

---
+++ delete from begin and end/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct node {
+    int data;
+    struct node *link;
+};
+int main()
+{
+    
+    return 0;
+}
